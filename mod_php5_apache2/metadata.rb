1dbecc3b6149bdc0f349cdf1900ce0cc51743949
matija <mmunjako@gmail.com>
2017-11-09 17:51:57 +0100

    b

---
+++ mod_php5_apache2/metadata.rb
@@ -0,0 +1,8 @@
+name        "mod_php5_apache2"
+description "Installs/Configures Apache with mod_php5"
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "apache2"
+depends "deploy"
