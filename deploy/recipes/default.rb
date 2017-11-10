3d5da16a800fa0ec86be1d8ea09fe14116627460
matija <mmunjako@gmail.com>
2017-11-10 10:21:34 +0100

    revert changes

--- deploy/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-include_recipe 'dependencies'
-
-node[:deploy].each do |application, deploy|
-  if 'flexcareers_workers' == params[:name]
-    port 8080
-    params[:server_port] = 8080
-  end
-end
--- mod_php5_apache2/metadata.rb
+++
@@ -1,8 +0,0 @@
-name        "mod_php5_apache2"
-description "Installs/Configures Apache with mod_php5"
-maintainer  "AWS OpsWorks"
-license     "Apache 2.0"
-version     "1.0.0"
-
-depends "apache2"
-# depends "deploy"
