a703e93b58b7c8e5138756a61c2bb41b60d0a4f6
matija <mmunjako@gmail.com>
2017-11-10 14:03:04 +0100

    ver

--- deploy/metadata.rb
+++ deploy/metadata.rb
@@ -2,7 +2,7 @@ name        "deploy"
 description "Deploy applications"
 maintainer  "AWS OpsWorks"
 license     "Apache 2.0"
-version     "1.0.0"
+# version     "1.0.0"
 
 depends "dependencies"
 depends "scm_helper"
